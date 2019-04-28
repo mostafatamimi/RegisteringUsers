c9c6fe4ee8d521d96ae16f4c9cf88f4e1646f409
mostafa <mostafatamimi@gmail.com>
2019-04-29 01:44:59 +0300

    Final Update

--- RegisteringUsers/Controllers/HomeController.cs
+++ RegisteringUsers/Controllers/HomeController.cs
@@ -30,6 +30,7 @@ namespace RegisteringUsers.Controllers
             return View();
         }
 
+        [Authorize]
         public ActionResult Contact()
         {
             ViewBag.Message = "Your contact page.";
