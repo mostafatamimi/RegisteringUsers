1a682ee615dbd7e0b3c1f985647fdcae63520947
mostafa <mostafatamimi@gmail.com>
2019-05-02 17:41:00 +0300

    Adding Chosen to Dropdown menu

--- RegisteringUsers/App_Start/BundleConfig.cs
+++ RegisteringUsers/App_Start/BundleConfig.cs
@@ -9,7 +9,8 @@ namespace RegisteringUsers
         public static void RegisterBundles(BundleCollection bundles)
         {
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-                        "~/Scripts/jquery-{version}.js"));
+                        "~/Scripts/jquery-{version}.js",
+                        "~/Scripts/chosen.jquery.min.js"));
 
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
                         "~/Scripts/jquery.validate*"));
@@ -24,7 +25,8 @@ namespace RegisteringUsers
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
-                      "~/Content/site.css"));
+                      "~/Content/site.css",
+                      "~/Content/chosen.css"));
         }
     }
 }
